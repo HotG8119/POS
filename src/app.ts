ed8924a36e5ff8f1a0426f497f3f483f024847ee
HotG8119 <k508060@gmail.com>
2023-10-07 02:10:49 +0800

    feat: add handlebars feature

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { type Request, type Response } from 'express'
+import handlebars from 'express-handlebars'
+import path from 'path'
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+// app.use(pages)
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World')
+  res.render('home')
 })
 
 app.listen(port, () => {
