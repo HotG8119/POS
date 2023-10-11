7b2566cb841e92ca2950610c54eada14510f90fa
HotG8119 <k508060@gmail.com>
2023-10-11 19:43:54 +0800

    fear: create home page

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,21 @@ import express, { type Request, type Response } from 'express'
 import handlebars from 'express-handlebars'
 import path from 'path'
 
+import handlebarsHelpers from './helpers/handlebars-helpers'
+
+import productData from './data/product.json'
+
 const app = express()
 const port = 3000
 
-app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 // app.use(pages)
 
 app.get('/', (req: Request, res: Response) => {
-  res.render('home')
+  res.render('home', { productData })
 })
 
 app.listen(port, () => {
