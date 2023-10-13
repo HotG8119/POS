c6d719bb02d9b2bae88393b60423f869820f3492
HotG8119 <k508060@gmail.com>
2023-10-13 21:43:53 +0800

    feat: add restaurant model

---
+++ migrations/20231013133917-create-product.js
@@ -0,0 +1,44 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        // 預設值為0
+        defaultValue: 0,
+        type: Sequelize.DECIMAL
+      },
+      tags: {
+        type: Sequelize.STRING
+      },
+      is_available: {
+        // 預設值為true
+        defaultValue: true,
+        type: Sequelize.BOOLEAN
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products')
+  }
+}
