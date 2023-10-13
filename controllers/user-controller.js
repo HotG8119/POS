018320aaf4fa630f5ae3cef7201c0e7df8d1099b
HotG8119 <k508060@gmail.com>
2023-10-13 18:40:17 +0800

    feat: passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,6 +9,18 @@ const userController = {
       if (err) return next(err)
       return res.redirect('/signin')
     })
+  },
+  logInPage: (req, res) => {
+    res.render('login')
+  },
+  logIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '成功登出！')
+    req.logout()
+    res.redirect('/login')
   }
 }
 module.exports = userController
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
 const { generalErrorHandler } = require('../../middleware/error-handler')
 
@@ -11,6 +12,9 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/login', userController.logInPage)
+router.post('/login', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.logIn)
+router.get('/logout', userController.logout)
 
 router.get('/', orderController.getProduct)
 router.use('/', generalErrorHandler)
