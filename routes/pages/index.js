e9be2b4c082e7dc27308abd06e26c63ce5841c61
HotG8119 <k508060@gmail.com>
2023-10-30 19:56:00 +0800

    feat: add past orders seed

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
-// const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
-const { authenticatedAdmin } = require('../../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
 const { generalErrorHandler } = require('../../middleware/error-handler')
 
 const admin = require('./modules/admin')
@@ -19,22 +18,22 @@ router.get('/signup', authenticatedAdmin, userController.signUpPage)
 router.post('/signup', authenticatedAdmin, userController.signUp)
 router.get('/login', userController.logInPage)
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), userController.logIn)
-router.get('/logout', userController.logout)
+router.get('/logout', authenticated, userController.logout)
 
-router.get('/products', productController.getProducts)
+router.get('/products', authenticated, productController.getProducts)
 
-router.put('/orders/unfinished/:id', orderController.finishOrder)
-router.get('/orders/unfinished', orderController.getUnfinishedOrdersPage)
+router.put('/orders/unfinished/:id', authenticated, orderController.finishOrder)
+router.get('/orders/unfinished', authenticated, orderController.getUnfinishedOrdersPage)
 
-router.get('/orders/unpaid', orderController.getUnpaidOrdersPage)
-router.put('/orders/checkout/cash/:id', orderController.checkoutByCash)
-router.post('/orders/checkout/linepay/:id', orderController.checkoutByLinepay)
-router.get('/orders/checkout/:id', orderController.getCheckoutPage)
+router.get('/orders/unpaid', authenticated, orderController.getUnpaidOrdersPage)
+router.put('/orders/checkout/cash/:id', authenticated, orderController.checkoutByCash)
+router.post('/orders/checkout/linepay/:id', authenticated, orderController.checkoutByLinepay)
+router.get('/orders/checkout/:id', authenticated, orderController.getCheckoutPage)
 
-router.get('/orders/today', orderController.getTodayOrdersPage)
-router.post('/orders', orderController.postOrder)
+router.get('/orders/today', authenticated, orderController.getTodayOrdersPage)
+router.post('/orders', authenticated, orderController.postOrder)
 
-router.get('/linepay/confirm', orderController.linepayConfirm)
+router.get('/linepay/confirm', authenticated, orderController.linepayConfirm)
 
 router.get('/', (req, res) => res.redirect('/products'))
 router.use('/', generalErrorHandler)
---
+++ seeders/20231030103628-order-seed-file.js
@@ -0,0 +1,77 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+
+const faker = require('faker')
+const dayjs = require('dayjs')
+
+function randomCartItem (products, qtyMax = 1) {
+  const qty = Math.floor(Math.random() * qtyMax) + 1
+  const cartItems = []
+  let totalAmount = 0
+
+  for (let i = 0; i < qty; i++) {
+    const randomProductNum = Math.floor(Math.random() * products.length)
+    const product = products[randomProductNum]
+    const quantity = Math.floor(Math.random() * 3) + 1
+
+    totalAmount += quantity * product.price
+    cartItems.push({
+      id: product.id,
+      quantity
+    })
+  }
+
+  return {
+    cartItems,
+    totalAmount
+  }
+}
+
+function randomPaymentMethod () {
+  if (Math.random() > 0.7) {
+    return 'cash'
+  } else {
+    return 'linepay'
+  }
+}
+
+function randomPastDate () {
+  const now = dayjs()
+  const randomDays = Math.floor(Math.random() * 60)
+  const past = now.subtract(randomDays, 'day')
+  const randomTime = faker.date.between(past.subtract(2, 'month').toDate(), past.toDate())
+  return randomTime
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const products = await queryInterface.sequelize.query('SELECT id,price FROM Products;', { type: queryInterface.sequelize.QueryTypes.SELECT })
+    const tables = await queryInterface.sequelize.query('SELECT id FROM Tables;', { type: queryInterface.sequelize.QueryTypes.SELECT })
+    const orders = []
+
+    // 新增200筆歷史訂單
+    for (let i = 0; i < 200; i++) {
+      const date = randomPastDate()
+      const { cartItems, totalAmount } = randomCartItem(products, 3)
+
+      orders.push({
+        cart_items: JSON.stringify(cartItems),
+        total_amount: totalAmount,
+        table_id: tables[Math.floor(Math.random() * tables.length)].id,
+        payment_method: randomPaymentMethod(),
+        created_at: date,
+        completed_at: dayjs(date).add(1, 'hour').toDate(),
+        paid_at: dayjs(date).add(2, 'hour').toDate(),
+        updated_at: dayjs(date).add(2, 'hour').toDate()
+      })
+    }
+
+    await queryInterface.bulkInsert('Orders', orders)
+    console.log('Orders seed file done!')
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Orders', {})
+  }
+}
