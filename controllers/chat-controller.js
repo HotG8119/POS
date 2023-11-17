c0c61bca2005bc90c139ad467328ea550661811b
HotG8119 <k508060@gmail.com>
2023-11-17 21:43:56 +0800

    Update dependencies and implement socket.io for real-time chat functionality

--- controllers/chat-controller.js
+++ controllers/chat-controller.js
@@ -1,6 +1,29 @@
+const dayjs = require('dayjs')
+
 const chatController = {
-  getChatRoomsPage: (req, res) => {
-    res.render('chat-rooms')
+  getChatRoomsPage: async (req, res) => {
+    try {
+      const io = req.app.io
+
+      io.once('connection', socket => {
+        console.log('使用者連接')
+        socket.on('error', console.error)
+
+        socket.on('chat message', msg => {
+          msg.value.time = dayjs().format('HH:mm')
+          io.emit('chat message', msg)
+        })
+
+        socket.on('disconnect', () => {
+          console.log('使用者離開')
+        }
+        )
+      })
+
+      res.render('chat-rooms')
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
