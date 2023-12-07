b51d64dfe932de9093487236cedae82d8d51e38a
HotG8119 <k508060@gmail.com>
2023-12-07 18:40:52 +0800

    feat: add category selector and refactor product services

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -4,7 +4,7 @@ const productController = {
   getProducts: (req, res, next) => {
     productServices.getProducts(req, (err, data) => {
       if (err) return next(err)
-      return res.render('products', { products: data.products, tables: data.tables })
+      return res.render('products', { products: data.products, tables: data.tables, categories: data.categories, categoryId: data.categoryId })
     })
   }
 }
