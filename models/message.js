fae7b71aa41ada0f352afce5f2b824e7aace52df
HotG8119 <k508060@gmail.com>
2023-12-07 22:05:33 +0800

    feat: store messages in database

--- models/message.js
+++ models/message.js
@@ -3,27 +3,25 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Message extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      User.hasMany(models.Order, { foreignKey: 'userId' })
-      User.hasMany(models.Message, { foreignKey: 'userId' })
+      Message.belongsTo(models.User, { foreignKey: 'userId' })
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
+  Message.init({
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    message_type: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
-    tableName: 'Users',
+    modelName: 'Message',
+    tableName: 'Messages',
     underscored: true
   })
-  return User
+  return Message
 }
---
+++ services/chat-services.js
@@ -0,0 +1,25 @@
+const { Message, User } = require('../models')
+
+const chatServices = {
+  getMessages: async (req, cb) => {
+    try {
+      const messages = await Message.findAll({
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'ASC']],
+        attributes: ['id', 'content', 'createdAt'],
+        include: [
+          User,
+          { model: User, attributes: ['name'] }
+        ]
+
+      })
+
+      return cb(null, messages)
+    } catch (err) {
+      return cb(err)
+    }
+  }
+}
+
+module.exports = chatServices
